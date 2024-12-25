849f0d3b6fb12bfa52aa22f866083e63e1a2b671
root <root@140.82.113.4github.com>
2024-12-25 20:13:38 +0800

    add linalg lib eigen

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ CC=g++
 RM=rm -rf 
 SRC=$(wildcard *.cc)
 OBJECT=$(patsubst %.cc, %.o, $(SRC))
-CXXFLAGS= -O2 -g -std=c++20
+CXXFLAGS= -O2 -g -std=c++20 -I 3rd_party/eigen -w
 LDFLAGS=-pthread
 
 all: $(TARGET)
--- main.cc
+++ main.cc
@@ -1,6 +1,23 @@
 #include <iostream>
+#include "Eigen/Dense"
+#include "Eigen/Core"
+#include "Eigen/LU"
+#include "Eigen/SVD"
+#include "Eigen/QR"
 
 int main() {
-    std::cout << "AI" << std::endl;
+    Eigen::Matrix<int, 2, 2> A;
+    A << 1, 2,
+         4, 8;
+    Eigen::Matrix<int, 2, 2> B;
+    B << 5, 6,
+         7, 8;
+    
+    std::cout << A << std::endl;
+    std::cout << "-----------------" << std::endl;
+    std::cout << B << std::endl;
+    auto C = A + B;
+    auto D = A * B;
+    std::cout << D << std::endl;
     return 0;
 }
