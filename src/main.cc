a56b75f681c092bf63182d7a646c64cedbc2509b
root <root@140.82.113.4github.com>
2024-12-25 21:08:51 +0800

    nn add sum and agn

--- src/main.cc
+++ src/main.cc
@@ -3,5 +3,8 @@
 
 int main() {
     std::cout << "AI" << std::endl;
+    NN<double> nn;
+    auto val = nn.agn(nn.sum(-10.5, 2.5, 2.5));
+    std::cout << "output:" << val << std::endl;
     return 0;
 }
--- src/nn.h
+++ src/nn.h
@@ -5,7 +5,16 @@ class NN {
 public:
     NN() {};
     ~NN() {};
+
     double agn(double value) {
         return value > 0 ? 1 : 0;
     }
+
+    double sum(double a1, double a2, double a3) {
+        return a1 * w1  + a2 * w2 + a3 * w3;
+    }
+private:
+    double w1{0.2};
+    double w2{0.3};
+    double w3{0.4};
 };
