e16bf3e9f1b26d113cb8425c179fa5482b195bc6
root <root@140.82.113.4github.com>
2024-12-25 20:45:04 +0800

    add nn

--- main.cc
+++ main.cc
@@ -4,6 +4,7 @@
 #include "Eigen/LU"
 #include "Eigen/SVD"
 #include "Eigen/QR"
+#include "nn.h"
 
 int main() {
     Eigen::Matrix<int, 2, 2> A;
@@ -12,12 +13,11 @@ int main() {
     Eigen::Matrix<int, 2, 2> B;
     B << 5, 6,
          7, 8;
-    
     std::cout << A << std::endl;
-    std::cout << "-----------------" << std::endl;
     std::cout << B << std::endl;
     auto C = A + B;
     auto D = A * B;
     std::cout << D << std::endl;
+    NN<int> nn;
     return 0;
 }
